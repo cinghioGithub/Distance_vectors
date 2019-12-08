08a57f94ffbc877fc652dd5704bfed2010ac2e6f
cinghioGithub <bravienrico@yahoo.it>
2019-12-08 19:01:06 +0100

    Added insVertex() method in GRAPH.py and loadGraph() in main.py in order to insert file information about graph in a data structure

--- GRAPH.py
+++ GRAPH.py
@@ -1,6 +1,23 @@
 #! /usr/bin/python3.7m
 
+import ST
+
 class GRAPH:
 
-    def __init__(self):
+    def __init__(self, n):
         self.mat = []
+        vertexNum = n
+        for i in range(n):
+            self.mat.append([])
+            for j in range(n):
+                self.mat[i].append(0)
+    
+    def insVertex(self, v1, v2, st):
+        n1 = st.getPosition(v1)
+        n2 = st.getPosition(v2)
+
+        self.mat[n1][n2] = 1
+        self.mat[n2][n1] = 1
+
+    def getMatrix(self):
+        return self.mat
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3.7m
 
 import ST
+import GRAPH
 
 def read_file(name, st):
     try:
@@ -16,10 +17,28 @@ def read_file(name, st):
         st.insert(strlist[0])
         st.insert(strlist[1])
 
+def loadGraph(name, st, g):
+    try:
+        fp = open(name, "r")
+    except:
+        print('Error on opening file\n')
+        quit()
+    
+    content = fp.readlines()
+
+    for line in content:
+        strlist = line.split()
+        g.insVertex(strlist[0], strlist[1], st)
+
 #main:
 
 st = ST.ST()
+
 name = 'file.txt'
 
 read_file(name, st)
 
+g = GRAPH.GRAPH(st.getDimention())
+loadGraph(name, st, g)
+
+#print(g.getMatrix(), st.getDimention())
